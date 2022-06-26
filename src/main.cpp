4efd86f39bce1aa101d4c90c867917465fc3a825
Janik Endtner <janik-endtner@hotmail.com>
2022-06-26 17:56:33 +0200

    add possibility to change to summer time

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,15 @@ auto board = new Board;
 int minBrightness = 20;
 int maxBrightness = 100;
 // we get max 1024 from input. set this value to reach 100% brightness earlier
-int brightestAtXBit = 800;
+int brightestAtXBit = 1024;
+
+int previousBrightness;
+int brightnessChanges = 0;
+bool summerTime = false;
 
 int roundDownToFive(int number);
-void calculateAndSetBrightness();
+int calculateBrightness();
+void calculateSummerTime(int second, int brightness);
 
 void setup() {
     delay(3000); // power-up safety delay
@@ -25,7 +30,7 @@ void setup() {
     }
     if (!rtc.isrunning()) {
         Serial.println("RTC is NOT running! Setting initial time...");
-        rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+        rtc.adjust(DateTime(2022, 6, 26, 16, 48, 0));
     }
     if (rtc.isrunning()){
         Serial.println("RTC is running.");
@@ -36,6 +41,11 @@ void loop() {
 
     DateTime now = rtc.now();
     int hour = now.hour();
+
+    if (summerTime) {
+        hour++;
+    }
+
     int minute = now.minute();
     Serial.println(now.timestamp());
 
@@ -51,7 +61,10 @@ void loop() {
     board->turnOnLightsForMinutes(minuteRounded);
     board->turnOnLightsForRest(rest);
 
-    calculateAndSetBrightness();
+    int brightness = calculateBrightness();
+    board->setBrightness(brightness);
+    calculateSummerTime(now.second(), brightness);
+    previousBrightness = brightness;
 
     board->show();
 
@@ -62,12 +75,35 @@ int roundDownToFive(int number) {
     return (number/5) * 5;
 }
 
-void calculateAndSetBrightness() {
+int calculateBrightness() {
     float analogValue = analogRead(A0);
     float percentage = analogValue / brightestAtXBit;
     float brightness = minBrightness + (maxBrightness - minBrightness) * percentage;
     brightness = min(100, brightness);
     Serial.print("Brightness: ");
     Serial.println(brightness);
-    board->setBrightness((int) brightness);
+    return (int) brightness;
+}
+
+/**
+ * Über das Abdecken des Helligkeitssensors soll es möglich sein, die Sommerzeit zu setzen
+ * oder wieder zu Winterzeit zu wechseln. Dafür muss der Sensor dreimal in kurzer Zeit
+ * verdunkelt und wieder erhellt werden.
+ */
+void calculateSummerTime(int second, int brightness) {
+    // alle dreissig sekunden reduzieren wir die Anzahl Wechsel
+    if (second % 30 == 0 && brightnessChanges > 0) {
+        brightnessChanges--;
+    }
+    // Wenn sich die Helligkeit um mindesten 20% verändert, werten wir dies als
+    // grosse Veränderung
+    float brightnessChange = abs((brightness - previousBrightness) / previousBrightness);
+    Serial.println(brightnessChanges);
+    if (brightnessChange > 0.2) {
+        brightnessChanges++;
+    }
+    if (brightnessChanges >= 3) {
+        summerTime = !summerTime;
+        brightnessChanges = 0;
+    }
 }
